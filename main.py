9718b113a89430bcd80d456f7714baaf4a45ea27
王冰轮 <1637328832@qq.com>
2022-09-20 20:42:22 +0800

    初始化1

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(torch.tensor(2,3))
+    print(torch.ones(4,4))
+    print(torch.zero(6,6))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
