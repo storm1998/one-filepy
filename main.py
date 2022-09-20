87fb5e71b53fcffc9b48f03524ac4af7aaa0d42e
王冰轮 <1637328832@qq.com>
2022-09-20 20:42:45 +0800

    初始化1

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import torch
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(torch.tensor(2,3))
     print(torch.ones(4,4))
     print(torch.zero(6,6))
 
