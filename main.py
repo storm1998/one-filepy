99a0ab09e93be112ccec87e817ab20b10e2e0ed9
zxz <2201083714@qq.com>
2022-09-20 16:26:31 +0800

    初始化1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+import torch
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print(torch.tensor(2,3))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
